3e176273dce1f13145baf706912afc368724945a
sxw031 <jameswangishere@gmail.com>
2017-02-09 23:49:13 -0800

    add init code to handlers

---
+++ handlers/__init__.py
@@ -0,0 +1,15 @@
+from blogbase import BaseHandler
+from blogmainpage import BlogMain
+from post import PostPage
+from newpost import NewPost
+from editpost import EditPost
+from deletepost import DeletePost
+from addcomment import AddComment
+from editcomment import EditComment
+from deletecomment import DeleteComment
+from likepost import LikePost
+from unlikepost import UnlikePost
+from signup import Signup
+from login import Login
+from logout import Logout
+
